f04fab15177f515bb4f91567c89ffc29369ac2d0
Ruan Augusto Bernardi Pasa <principalruanbernardi@gmail.com>
2023-12-21 15:14:04 -0300

    Update: lazy-images.js functions
    - Updates and addition of new functions.
    - Automatic detection of image extensions.
    - Some settings can be changed easily now.

--- lazy-images.js
+++ lazy-images.js
@@ -1,3 +1,52 @@
+// Dev must change this vars.
+const isImageResponsive = true;
+const imagePath = './images/';
+
+const imageExtension = ['webp', 'png', 'jpg', 'jpeg', 'svg'];
+
+function generateImage(imgNameId, isImageResponsive, callback) {
+  let imgMatch = false;
+
+  for(var i = 0; i < imageExtension.length; i++) {
+    let ext = imageExtension[i];
+    const imgSource = `${imagePath}${imgNameId}.${ext}`;
+
+    fileExists(imgSource, function (exists) {
+      if (exists && !imgMatch) {
+        imgMatch = true;
+        let imgResult;
+
+        if (isImageResponsive) {
+          imgResult = `
+            <picture>
+              <source media="(min-width: 1200px)" srcset="${imgSource}">
+              <source media="(min-width: 768px)" srcset="${imgSource}">
+              <source media="(min-width: 320px)" srcset="${imgSource}">
+              <img loading="lazy" src="${imgSource}">
+            </picture>
+          `;
+        } else {
+          imgResult = `
+            <img loading="lazy" src="${imgSource}">
+          `;
+        }
+        callback(imgResult);
+      }
+    });
+  }
+}
+
+function fileExists(filePath, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      callback(xhr.status === 200);
+    }
+  };
+  xhr.open('HEAD', filePath, true);
+  xhr.send();
+}
+
 function isSectionVisible(element) {
   let { top, bottom, left, right } = element.getBoundingClientRect(); 
 
@@ -31,9 +80,9 @@ function getDocumentElements() {
     imgDiv = section.getElementsByClassName('img');
   
     for (var i = 0; i < imgDiv.length; i++) {
-      let divImgId = imgDiv[i];
+      let divImgNameId = imgDiv[i];
 
-      sectionInfoObject.images.push(divImgId);
+      sectionInfoObject.images.push(divImgNameId);
     };
   
     sectionImgs.push(sectionInfoObject);
@@ -46,20 +95,14 @@ function loadImages(sectionImgs) {
   sectionImgs.forEach(section => {
     if (isSectionVisible(section.sectionTag) && !section.sectionTag.classList.contains('processed')) { 
       section.images.forEach(img => {
-        let imgId = img.attributes.id.nodeValue;
-        let imgDivElement = document.getElementById(imgId); 
-        
+        let imgNameId = img.attributes.id.nodeValue;
+        let imgDivElement = document.getElementById(imgNameId); 
+
         if (imgDivElement) {
-          imgDivElement.innerHTML = 
-          `
-            <picture>
-              <source media="(min-width: 1200px)" srcset="./images/${imgId}.webp">
-              <source media="(min-width: 768px)" srcset="./images/${imgId}.webp">
-              <source media="(min-width: 320px)" srcset="./images/${imgId}.webp">
-              <img loading="lazy" src="./images/${imgId}.webp">
-            </picture>
-          `;
-        }
+          generateImage(imgNameId, isImageResponsive, function (html) {
+            imgDivElement.innerHTML = html;
+          });
+        };
       });
       section.sectionTag.classList.add('processed');
     }
